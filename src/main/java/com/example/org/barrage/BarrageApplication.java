d173fd30fa9290d0014c2aa72f465200dd09758d
hlzhan <hlzhan@hlzhandeMBP.lan>
2021-02-05 19:43:59 +0800

    [Honglai] : init commit

---
+++ src/main/java/com/example/org/barrage/BarrageApplication.java
@@ -0,0 +1,13 @@
+package com.example.org.barrage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarrageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarrageApplication.class, args);
+	}
+
+}
