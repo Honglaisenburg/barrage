7618c99673c269f9b428f551fc60ad2cbc1ed5a3
hlzhan <honglaisenburg@qq.com>
2021-02-06 22:50:25 +0800

    [Honglai] : add 8 instances

--- start-server.sh
+++ start-server.sh
@@ -1,5 +1,8 @@
-nohup java -jar ./build/libs/barrage-0.0.1-SNAPSHOT.jar --.server.port=8080 >/home/null  &
-nohup java -jar ./build/libs/barrage-0.0.1-SNAPSHOT.jar --.server.port=8081 >/home/null  &
-nohup java -jar ./build/libs/barrage-0.0.1-SNAPSHOT.jar --.server.port=8082 >/home/null  &
-nohup java -jar ./build/libs/barrage-0.0.1-SNAPSHOT.jar --.server.port=8083 >/home/null  &
-nohup java -jar ./build/libs/barrage-0.0.1-SNAPSHOT.jar --.server.port=8084 >/home/null  &
+nohup java -jar ./build/libs/barrage-0.0.1-SNAPSHOT.jar --server.port=8080 >/home/null  &
+nohup java -jar ./build/libs/barrage-0.0.1-SNAPSHOT.jar --server.port=8081 >/home/null  &
+nohup java -jar ./build/libs/barrage-0.0.1-SNAPSHOT.jar --server.port=8082 >/home/null  &
+nohup java -jar ./build/libs/barrage-0.0.1-SNAPSHOT.jar --server.port=8083 >/home/null  &
+nohup java -jar ./build/libs/barrage-0.0.1-SNAPSHOT.jar --server.port=8084 >/home/null  &
+nohup java -jar ./build/libs/barrage-0.0.1-SNAPSHOT.jar --server.port=8085 >/home/null  &
+nohup java -jar ./build/libs/barrage-0.0.1-SNAPSHOT.jar --server.port=8086 >/home/null  &
+nohup java -jar ./build/libs/barrage-0.0.1-SNAPSHOT.jar --server.port=8087 >/home/null  &
