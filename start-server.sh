603366b3f5f7e1dad6831d0e72245b4dafdc8b18
hlzhan <honglaisenburg@qq.com>
2021-02-06 22:46:19 +0800

    [Honglai] : start up 5 servers instead of one

--- start-server.sh
+++ start-server.sh
@@ -1,1 +1,5 @@
-nohup java -jar ./build/libs/barrage-0.0.1-SNAPSHOT.jar >/home/null  &
+nohup java -jar ./build/libs/barrage-0.0.1-SNAPSHOT.jar --.server.port=8080 >/home/null  &
+nohup java -jar ./build/libs/barrage-0.0.1-SNAPSHOT.jar --.server.port=8081 >/home/null  &
+nohup java -jar ./build/libs/barrage-0.0.1-SNAPSHOT.jar --.server.port=8082 >/home/null  &
+nohup java -jar ./build/libs/barrage-0.0.1-SNAPSHOT.jar --.server.port=8083 >/home/null  &
+nohup java -jar ./build/libs/barrage-0.0.1-SNAPSHOT.jar --.server.port=8084 >/home/null  &
