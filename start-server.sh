ed9009b2d8b0bd49258c0529489a5d5d395b4015
Honglaisenburg <71200338+Honglaisenburg@users.noreply.github.com>
2022-10-19 14:16:33 +0800

    update start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -1,3 +1,5 @@
+echo 'starting server'
+
 nohup java -jar ./build/libs/barrage-0.0.1-SNAPSHOT.jar --server.port=8080 >/home/null  &
 nohup java -jar ./build/libs/barrage-0.0.1-SNAPSHOT.jar --server.port=8081 >/home/null  &
 nohup java -jar ./build/libs/barrage-0.0.1-SNAPSHOT.jar --server.port=8082 >/home/null  &
